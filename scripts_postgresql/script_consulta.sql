bcc3a81f8f29ec5f75825ec068cd0a39a130b0e3
geiziane-sg <109633987+geiziane-sg@users.noreply.github.com>
2025-12-08 14:42:45 -0300

    Alterações na Consulta

--- scripts_postgresql/script_consulta.sql
+++ scripts_postgresql/script_consulta.sql
@@ -1,12 +1,14 @@
-SELECT Ninja.nome AS nome_ninja,
-    COUNT(tm.id_missao) AS total_missoes,
-    SUM(CASE WHEN Missao.ranking = 'S' THEN 1 ELSE 0 END) AS missoes_rank_s,
-    ROUND(
-        CAST(SUM(CASE WHEN Missao.ranking = 'S' THEN 1 ELSE 0 END) AS NUMERIC) / 
-        NULLIF(COUNT(tm.id_missao), 0) * 100, 2
-    ) AS porcentagem_rank_s
-FROM Ninja 
+SELECT 
+    Ninja.id_ninja,
+    Vila.id_vila,
+    tm.id_time,
+    Missao.id_missao,
+    COUNT(*) OVER (PARTITION BY Ninja.id_ninja, Vila.id_vila) AS total_missoes,
+    ROUND(CAST(1 AS NUMERIC) / COUNT(*) OVER (PARTITION BY Ninja.id_ninja, Vila.id_vila) * 100, 2) AS pct_s_ninja
+FROM Ninja
+INNER JOIN Vila ON Ninja.id_vila = Vila.id_vila
 INNER JOIN Ninja_Time nt ON Ninja.id_ninja = nt.id_ninja
 INNER JOIN Time_Missao tm ON nt.id_time = tm.id_time
 INNER JOIN Missao ON tm.id_missao = Missao.id_missao
-GROUP BY Ninja.nome;
+WHERE Missao.ranking = 'S'
+ORDER BY Ninja.id_ninja, tm.id_time, Missao.id_missao;
